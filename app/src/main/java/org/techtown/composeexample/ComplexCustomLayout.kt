b07611a0dbb31546ac37f5044cac69add5d2885e
Olio <jgg0819@naver.com>
2021-08-19 23:17:49 +0900

    custom staggedgrid

---
+++ app/src/main/java/org/techtown/composeexample/ComplexCustomLayout.kt
@@ -0,0 +1,144 @@
+package org.techtown.composeexample
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.background
+import androidx.compose.foundation.horizontalScroll
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.Card
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.Layout
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.unit.dp
+import com.google.android.material.chip.Chip
+import org.techtown.composeexample.ui.theme.ComposeExampleTheme
+import kotlin.math.max
+
+class ComplexCustomLayout : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            ComposeExampleTheme {
+                ComplexBodyContent()
+            }
+        }
+    }
+}
+
+@Composable
+fun StaggeeredGrid(
+    modifier: Modifier = Modifier,
+    rows: Int = 3,
+    content: @Composable () -> Unit
+){
+    Layout(modifier = modifier, content = content ){ measurables, constraints ->
+
+        // 각열 width
+        val rowWidths = IntArray(rows){0}
+
+        // 각열 height
+        val rowHeights = IntArray(rows){0}
+
+        val placeables = measurables.mapIndexed { index, measurable ->
+
+            //Mesure each child
+            val placeable = measurable.measure(constraints)
+
+            //Tracks the width and max height of each now
+            val row = index % rows
+            rowWidths[row] += placeable.width
+            rowHeights[row] = max(rowHeights[row],placeable.height)
+
+            placeable
+        }
+
+        val width = rowWidths.maxOrNull()?.coerceIn(constraints.maxWidth.rangeTo(constraints.maxWidth))
+            ?: constraints.minWidth
+
+        val height = rowHeights.sumOf { it }.coerceIn(constraints.minHeight.rangeTo(constraints.maxHeight))
+
+        val rowY = IntArray(rows){0}
+        for(i in 1 until rows){
+            rowY[i] = rowY[i - 1] + rowHeights[i - 1]
+        }
+
+        layout(width, height) {
+            val rowX = IntArray(rows) { 0 }
+
+            placeables.forEachIndexed { index, placeable ->
+                val row = index % rows
+                placeable.placeRelative(
+                    x = rowX[row],
+                    y = rowY[row]
+                )
+                rowX[row] += placeable.width
+            }
+        }
+
+    }
+
+}
+
+@Composable
+fun Chip(modifier: Modifier = Modifier, text: String) {
+    Card(
+        modifier = modifier,
+        border = BorderStroke(color = Color.Black, width = Dp.Hairline),
+        shape = RoundedCornerShape(8.dp)
+    ) {
+        Row(modifier = Modifier.padding(start = 8.dp, top = 4.dp, end = 8.dp, bottom = 4.dp),
+            verticalAlignment = Alignment.CenterVertically)
+        {
+            Box(modifier = Modifier
+                .size(16.dp, 16.dp)
+                .background(color = MaterialTheme.colors.secondary))
+            Spacer(modifier = Modifier.width(4.dp))
+            Text(text = text)
+        }
+    }
+}
+
+@Preview
+@Composable
+fun ChipPreview() {
+    ComposeExampleTheme {
+        Chip(text = "Hi there")
+    }
+}
+
+val topics = listOf(
+    "Arts & Crafts", "Beauty", "Books", "Business", "Comics", "Culinary",
+    "Design", "Fashion", "Film", "History", "Maths", "Music", "People", "Philosophy",
+    "Religion", "Social sciences", "Technology", "TV", "Writing"
+)
+
+@Composable
+fun ComplexBodyContent(modifier: Modifier = Modifier) {
+    Row(modifier = modifier.horizontalScroll(rememberScrollState())) {
+        StaggeeredGrid(modifier = modifier, rows = 5) {
+            for (topic in topics) {
+                //각 card 마다 padding 및 text주기.
+                Chip(modifier = Modifier.padding(8.dp), text = topic)
+            }
+        }
+    }
+}
+
+@Preview
+@Composable
+fun ComplexCustomPreview() {
+    ComposeExampleTheme {
+        ComplexBodyContent()
+    }
+}
